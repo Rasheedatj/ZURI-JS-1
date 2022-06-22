b68e01eb4e337c49a7cbb610f4b37cbc135c61dc
Rasheedat Jinadu <jinaduikeoluwa@gmail.com>
2022-06-22 10:56:38 -0700

    first js commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+This ia a code that shows my name, height and country in the web browser
---
+++ zuri.js
@@ -0,0 +1,7 @@
+let name ='My name is Jinadu Rasheedat Ikeoluwa';
+let height =' I am  5.7 tall';
+let country =' I am from Nigeria and I also live here';
+
+alert(name);
+alert(height);
+alert(country)
